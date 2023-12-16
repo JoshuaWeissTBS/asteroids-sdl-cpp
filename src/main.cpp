717e2bc0adb49726e09bcb5dfc24ffe3cc70bedb
Joshua Weiss <joshua.weiss@hotmail.com>
2023-12-16 13:49:26 -0800

    moved input to switch case

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,29 @@ void _input()
 {
     SDL_Event event;
     while(SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT)
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                game_running = false;
+                break;
+            case SDL_MOUSEMOTION:
+                cout << "Mouse moved" << " " << event.motion.x << "," << event.motion.y << endl;
+                break;
+            case SDL_KEYDOWN:
+                cout << "Key pressed" << event.key.keysym.scancode << endl;
+                break;
+        }
+
+        // Keyboard inputs
+        const Uint8 *state = SDL_GetKeyboardState(NULL);
+        if (state[SDL_SCANCODE_ESCAPE])
+        {
             game_running = false;
-        if (event.type == SDL_MOUSEMOTION)
-            cout << "Mouse moved" << " " << event.motion.x << "," << event.motion.y << endl;
-        if (event.type == SDL_KEYDOWN)
-            cout << "Key pressed" << event.key.keysym.scancode << endl;
+        }
+        if (state[SDL_SCANCODE_LEFT])
+        {
+            cout << "Left arrow pressed" << endl;
+        }
     }
 }
 
@@ -27,8 +44,8 @@ int main()
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow("Asteroids", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_RESIZABLE);
-    if (window == NULL)
+    SDL_Window *sdl_window = SDL_CreateWindow("Asteroids", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_VULKAN);
+    if (sdl_window == NULL)
     {
         cout << "SDL_CreateWindow failed: " << SDL_GetError() << endl;
         return 1;
@@ -40,7 +57,7 @@ int main()
         _input();
     }
 
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow(sdl_window);
     SDL_Quit();
 
     return 0;
